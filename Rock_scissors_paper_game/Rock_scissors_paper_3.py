eb877d9f806abd70885f0280d75806d970e95536
potato potage <f.potato.potage@gmail.com>
2025-01-23 17:57:31 +0900

    0123_1

--- Rock_scissors_paper_game/Rock_scissors_paper_3.py
+++ Rock_scissors_paper_game/Rock_scissors_paper_3.py
@@ -15,15 +15,21 @@ def user_choice():
     if user_input_choice == '1':
         result_rsp(user_input_choice)
     elif user_input_choice == "2":
+        score_count()
         print('프로그램이 종료됩니다')
     else:
         print('알맞은 숫자를 입력해주세요')
         user_choice()
 
 
+win_count = 0
+tie_count = 0
+lose_count = 0
 
 
 def result_rsp(user_input):
+    
+    global win_count, tie_count, lose_count
 
     print('가위바위보!')
 
@@ -32,29 +38,53 @@ def result_rsp(user_input):
     if user_input == '가위':
         if result == '가위':
             print('비겼습니다')
+            tie_count += 1
         elif result == '바위':
             print('이겼습니다')
+            win_count += 1
         elif result == '보':
             print('졌습니다')
+            lose_count += 1
     elif user_input == '바위':
         if result == '가위':
             print('졌습니다')
+            lose_count += 1
         elif result == '바위':
             print('비겼습니다')
+            tie_count += 1
         elif result == '보':
             print('이겼습니다')
+            win_count += 1
     elif user_input == '보':
         if result == '보':
             print('비겼습니다')
+            tie_count += 1
         elif result == '가위':
             print('이겼습니다')
         elif result == '바위':
+            win_count += 1
             print('졌습니다')
+            lose_count += 1
     else:
         print('알맞은 단어를 입력해주세요')
         result_rsp(user_input)
 
     user_choice()
+
+    return win_count, tie_count, lose_count
+
+
+def score_count():
+    global win_count, lose_count, tie_count
+
+    print('')
+    print('승적')
+    print('-------')
+    print(f'이긴 횟수: {win_count}')
+    print(f'비긴 횟수: {tie_count}')
+    print(f'진 횟수: {lose_count}')
+    print('-------')
+    print('')
 
 
 user_choice()
