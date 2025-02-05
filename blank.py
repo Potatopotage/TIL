8224d58750817f8ad38f3f55215bbd2ca8990e21
potatopotage <f.potato.potage@gmail.com>
2025-02-05 17:49:01 +0900

    0205_1

--- blank.py
+++ blank.py
@@ -1,2 +1,12 @@
-a = 'ddsf'
-print(list(a))
+import ast
+
+arr = ast.literal_eval((input()))
+
+n = len(arr)
+
+for i in range(n - 1, 0, - 1):
+    for j in range(i):
+        if arr[j] > arr[j + 1]:
+            arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+    print(f'패스 ({n - i}) 후 : {arr}')
